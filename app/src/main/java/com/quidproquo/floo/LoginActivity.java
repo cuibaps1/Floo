3f2376787b71a9cb0d9e2f6348579f7d46781b7b
cuibaps1 <jadanngo@gmail.com>
2017-08-08 11:56:17 -0700

    switch to register activity

--- app/src/main/java/com/quidproquo/floo/LoginActivity.java
+++ app/src/main/java/com/quidproquo/floo/LoginActivity.java
@@ -1,13 +1,50 @@
 package com.quidproquo.floo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private TextView textViewSignin;
+    private TextView textViewSignup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextUsername = (EditText) findViewById(R.id.username);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        textViewSignin = (TextView) findViewById(R.id.signin);
+        textViewSignup = (TextView) findViewById(R.id.signup);
+
+        textViewSignin.setOnClickListener(this);
+        textViewSignup.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == textViewSignup){
+            Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(intent);
+        }
+
+        if(v == textViewSignin){
+            //open login activity
+        }
+
+
     }
 }
